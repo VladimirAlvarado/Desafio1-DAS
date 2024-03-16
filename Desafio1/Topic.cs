26b7a031f52e8d4e6bdfb758841d10b34f17c59e
VladimirAlvarado <vladimiralvarado001@gmail.com>
2024-03-16 14:56:52 -0600

    COMMIT DE PROYECTO FINAL
    se sube el proyecto final para la revisión de todos
    
    Co-Authored-By: Alvaro Raymundo <131578814+alvaro-raymundo@users.noreply.github.com>
    Co-Authored-By: Carlos0295 <145521914+Carlos0295@users.noreply.github.com>

---
+++ Desafio1/Topic.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio1
+{
+    internal class Topic
+    {
+        public string Logo { get; set; }
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public List<string> Country { get; set; }
+    }
+}
